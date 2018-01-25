ccab6b019d3d2a3e5f8b36f7b50c989a820abc03
beommaker <beom.maker@gmail.com>
2018-01-25 20:48:41 +0900

    feat(blog): done
    https://app.asana.com/0/513674312489999/529282831944992

--- blog/views.py
+++ blog/views.py
@@ -42,3 +42,27 @@ def post_new(request):
         form = PostForm()
 
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+
+        form = PostForm(instance=post)
+
+        if form.is_valid():
+
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+
+            post.save()
+
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
